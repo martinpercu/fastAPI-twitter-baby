ade53099a6e888fa26194c08d99bba0b7282a734
Martin Mendez <martinpercu@gmail.com>
2022-07-12 14:41:17 +0200

    Models for users

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
+# Python
+from typing import Optional
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel, EmailStr
+from pydantic import Field
+
+
+# FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+    
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+    
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=48
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=48
+    )
+    birth_date: Optional[date] = Field(default=None)
+    
+
+class Tweet(BaseModel):
+    pass
+
 @app.get(path="/")
 def home():
-    return {"baby-twiteer": "Working"}
+    return {"baby-twitter": "Working OK and Happy"}
